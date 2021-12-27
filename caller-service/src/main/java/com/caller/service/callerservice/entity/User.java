c3e562d050d2f930b43e27ef6cd7d3f3b7a94c78
Michle99 <sammiyonyx@gmail.com>
2021-12-27 11:32:20 -0500

    Adding a caller service

---
+++ caller-service/src/main/java/com/caller/service/callerservice/entity/User.java
@@ -0,0 +1,42 @@
+package com.caller.service.callerservice.entity;
+import java.util.List;
+
+public class User {
+	private Integer id;
+	private String name;
+	private Integer age;
+	
+	Float salary;
+	private List<Address> addresses;
+	
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
