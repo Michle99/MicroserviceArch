f94162dcbc4f6f187ed24d7b1a9e773865183df0
Michle99 <sammiyonyx@gmail.com>
2022-01-08 14:07:08 -0500

    Adding changes

--- airline/src/main/java/com/airline/airline/AirlineApplication.java
+++ airline/src/main/java/com/airline/airline/AirlineApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 
 
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class AirlineApplication {
 
--- airline/src/main/java/com/airline/airline/configs/WebSecurityConfig.java
+++ airline/src/main/java/com/airline/airline/configs/WebSecurityConfig.java
@@ -1,18 +1,46 @@
 package com.airline.airline.configs;
 
+
+import org.apache.naming.factory.DataSourceLinkFactory.DataSourceHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    @Autowired
+    private UserDetailsService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/users").authenticated()
+            
+            // .antMatchers("/", "/flight/search","/flight/book/verify", "/flight/book/cancel", "/img/**").permitAll()
+            // .antMatchers( "/flight/book**", "/flight/book/new").hasRole("AGENT")
+            // .antMatchers("/**").hasRole("ADMIN")
+            
             .anyRequest().permitAll()
             .and()
             .formLogin()
